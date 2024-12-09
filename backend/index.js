3517095325a53f44c002a305621c93c3d9f89be4
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-08 21:37:13 -0300

    getting todos

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,8 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-app.listen(8000, () => {
-  console.log("server has started on port 8000");
+app.listen(8020, () => {
+  console.log("server has started on port 8020");
 });
 
 //ROUTES
@@ -18,16 +18,37 @@ app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todoes (description) VALUES($1)",
+      "INSERT INTO todoes (description) VALUES($1) RETURNING *",
       [description]
     );
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 //get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todoes");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todoes WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //update a todo
 
